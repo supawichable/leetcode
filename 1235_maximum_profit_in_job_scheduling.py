aeff5fc109a1e3af3c04087929494e5d718b6ea5
Supawich Jiarakul <s.jiarakul@gmail.com>
2022-11-26 15:49:32 +0900

    edit 1235

--- 1235_maximum_profit_in_job_scheduling.py
+++ 1235_maximum_profit_in_job_scheduling.py
@@ -4,6 +4,9 @@ https://leetcode.com/problems/maximum-profit-in-job-scheduling/
 '''
 
 
+import heapq
+
+
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         mem = dict()
@@ -17,3 +20,16 @@ class Solution:
             mem[i] = max(jobs[i][2] + dfs(k), dfs(i+1))
             return mem[i]
         return dfs(0)
+
+# heap
+class Solution2:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = sorted([(startTime[i], endTime[i], profit[i]) for i in range(len(startTime))])
+        heap = []
+        max_profit = 0
+        for start, end, prof in jobs:
+            while heap and heap[0][0] <= start:
+                max_profit = max(max_profit, heap[0][1])
+                heapq.heappop(heap)
+            heapq.heappush(heap, (end, prof + max_profit))
+        return max([x[1] for x in heap])

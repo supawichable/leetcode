cfe9a4dc4d73c77e65d3646148ffe5b275214f0d
Supawich Jiarakul <s.jiarakul@gmail.com>
2022-11-28 13:04:36 +0900

    add 2225

--- 2225_find_playersa_with_zero_or_one_losses.py
+++ 2225_find_playersa_with_zero_or_one_losses.py
@@ -6,18 +6,18 @@ https://leetcode.com/problems/find-players-with-zero-or-one-losses/
 
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        lost0, lost1 = [], []
-        lost_count = defaultdict(int)
-        track_player = set()
-        for match in matches:
-            track_player.add(match[0])
-            track_player.add(match[1])
-            lost_count[match[1]] += 1
-        for i in track_player:
-            if i not in lost_count:
-                lost0.append(i)
-            elif lost_count[i] == 1:
-                lost1.append(i)
-        lost0.sort()
-        lost1.sort()
-        return [lost0, lost1]
+        ans0, ans1 = set(), set()
+        lost2 = set()
+        
+        for winner, loser in matches:
+            if winner not in lost2 and winner not in ans1:
+                ans0.add(winner)
+            if loser in ans0:
+                ans0.remove(loser)
+                ans1.add(loser)
+            elif loser in ans1:
+                ans1.remove(loser)
+                lost2.add(loser)
+            elif loser not in lost2:
+                ans1.add(loser)
+        return [sorted(list(ans0)), sorted(list(ans1))]
